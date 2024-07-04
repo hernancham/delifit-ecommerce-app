f14ac74b431c9f671da97bca8fd0b510d8210eaa
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-07-04 17:16:22 -0500

    chore: Remove commented code for ReactQueryDevtools in QueryProvider

--- src/components/context/QueryProvider.tsx
+++ src/components/context/QueryProvider.tsx
@@ -2,14 +2,14 @@
 import { useState } from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+/* import { ReactQueryDevtools } from "@tanstack/react-query-devtools"; */
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
