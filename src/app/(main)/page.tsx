050c1e5696d990f2a8565da771c98f51b0f105fc
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-07-04 17:29:48 -0500

    feat: Actualizar texto en la página principal

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ export default async function Page() {
   return (
     <div className='flex items-center justify-center'>
       <div className='flex flex-col gap-4'>
-        <h1>Home: </h1>
+        <h1>Prueba de Session en la ruta base: </h1>
         <pre>{JSON.stringify(session, null, 2)}</pre>
       </div>
     </div>
