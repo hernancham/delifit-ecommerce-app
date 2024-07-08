de20147e5c2dda337af37ff7b52e3f762e07f153
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-07-08 14:32:45 -0500

    feat: Update container padding in insumos page

--- src/app/(admin)/dashboard/insumos/page.tsx
+++ src/app/(admin)/dashboard/insumos/page.tsx
@@ -3,7 +3,7 @@ import { TablaInsumos } from "./_components/TablaInsumos";
 
 export default function Page() {
   return (
-    <div className='container mx-auto py-10'>
+    <div className='container mx-auto py-6'>
       <HeaderInsumos />
       <TablaInsumos />
     </div>
