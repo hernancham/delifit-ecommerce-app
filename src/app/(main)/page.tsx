aab127010a15603a6668b7e8e44739884a4ae204
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-07-11 11:47:07 -0500

    feat: Actualizar imagen de usuario en OptionsAuth

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,7 +6,6 @@ import { Testimonial } from "./_components/Testimonial";
 import { Newsletter } from "./_components/Newsletter";
 
 export default async function Page() {
-  const session = await auth();
   return (
     <div>
       <Hero />
@@ -17,13 +16,3 @@ export default async function Page() {
     </div>
   );
 }
-
-/* 
-      <div className='flex items-center justify-center'>
-      <div className='flex flex-col gap-4'>
-        <h1>Prueba de Deploy en Vercel: </h1>
-        <pre>{JSON.stringify(session, null, 2)}</pre>
-        <span>Delifit App Test</span>
-      </div>
-    </div> 
-    */
