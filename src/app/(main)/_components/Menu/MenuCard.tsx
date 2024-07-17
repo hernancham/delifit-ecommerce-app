b92b5ec87796d65964697d583e535758b5dcccb2
Josue07 <ejperazac@unjbg.edu.pe>
2024-07-16 19:17:52 -0500

    Modificando estilos de Menu y Productos

--- src/app/(main)/_components/Menu/MenuCard.tsx
+++ src/app/(main)/_components/Menu/MenuCard.tsx
@@ -10,16 +10,16 @@ export const MenuCard = ({ producto }: { producto: Producto }) => {
   const addToCartProducto = useCartStore((state) => state.addToCartProducto);
   return (
     <div className='flex flex-col gap-4 py-8 px-6 mx-4 rounded-xl'>
-      <div className='mb-3 flex justify-center'>
+      <div className='mb-3 flex justify-center h-[250px]'>
         <img
           src={producto.img_url}
           alt='menu'
-          className='rounded-full w-auto sm:max-w-[200px] md:max-w-[250px] shadow-1'
+          className='rounded-full w-auto h-full object-cover sm:max-w-[200px] md:max-w-[250px] shadow-1'
         />
       </div>
       <div className='flex flex-col items-center gap-4'>
-        <div className='space-y-3 text-center'>
-          <h1 className='text-xl'>{producto.nombre}</h1>
+        <div className='space-y-6 text-center'>
+          <h1 className='text-xl h-[70px] w-[200px]'>{producto.nombre}</h1>
           <p className='text-3xl font-semibold'>
             <span className='text-2xl font-cursive'></span> S/.{" "}
             {producto.precio_base}
@@ -33,7 +33,7 @@ export const MenuCard = ({ producto }: { producto: Producto }) => {
                 precio: producto.precio_base,
               })
             }
-            className='bg-lime-500 text-white w-full'
+            className='bg-graphite-deep text-white w-full hover:bg-graphite-dark dark:bg-green_p-dark dark:text-graphite-deep dark:hover:bg-green_p'
           >
             <ShoppingBasket className='size-6 mr-2' /> Agregar al carrito
           </Button>
--- src/app/(main)/menu/productos/_components/CardProduto.tsx
+++ src/app/(main)/menu/productos/_components/CardProduto.tsx
@@ -11,19 +11,20 @@ export const CardProducto = ({ producto }: { producto: Producto }) => {
   const addToCartProducto = useCartStore((state) => state.addToCartProducto);
 
   return (
-    <div className='min-w-[200px] max-w-xs bg-white rounded-xl  duration-500 hover:scale-105 hover:shadow-xl shadow-md flex flex-col justify-between dark:bg-slate-600'>
+    <div className='min-w-[200px] max-w-xs bg-beige-light dark:bg-graphite-dark rounded-xl  duration-500 hover:scale-105 hover:shadow-xl shadow-md flex flex-col justify-between'>
       <div className='relative'>
         <img
           src={producto.img_url}
           alt={producto.nombre}
           className='w-full aspect-square object-cover rounded-t-xl '
         />
-        <h3 className='absolute inset-x-0 bottom-0 block text-lg font-bold capitalize text-black p-4 bg-lime-100 bg-opacity-35'>
+        <div className='absolute inset-0 bg-gradient-to-t from-beige-light via-transparent to-transparent rounded-t-xl dark:from-graphite-dark'></div>
+        <h3 className='absolute inset-x-0 bottom-0 block text-lg font-bold capitalize text-black dark:text-white p-4 shadow'>
           {producto.nombre}
         </h3>
       </div>
       <div className='flex flex-col items-center gap-2 p-2'>
-        <div className='bg-gray-100 rounded-md p-1 dark:bg-green-900'>
+        <div className='text-beige-dark dark:text-green_p-dark rounded-md p-1'>
           {producto.cat_producto.nombre}
         </div>
         <div>
@@ -38,7 +39,7 @@ export const CardProducto = ({ producto }: { producto: Producto }) => {
               precio: producto.precio_base,
             })
           }
-          className='bg-lime-500 text-white w-full'
+          className='bg-graphite-deep text-white w-full hover:bg-graphite-dark dark:bg-green_p-dark dark:text-graphite-deep dark:hover:bg-green_p w-full'
         >
           <ShoppingBasket className='size-6 mr-2' /> Agregar al carrito
         </Button>
