57d2436dc6fcc4a1cc33f46223e023afac3eef7a
Hernan_Cham <hachambillac@unjbg.edu.pe>
2024-12-03 10:18:51 -0500

    Agregar mensajes de bienvenida y roles para usuarios autenticados en RolOptions

--- src/app/(main)/_components/RolOptions/index.tsx
+++ src/app/(main)/_components/RolOptions/index.tsx
@@ -32,19 +32,33 @@ export const RolOptions = () => {
       {/* {status === "unauthenticated" && <div>No estas logueado</div>}
       {status === "loading" && <div>Cargando...</div>} */}
       <div className='w-full max-w-3xl grid grid-cols-1 gap-6'>
-        {status === "authenticated" && (
+        {status === "authenticated" ? (
           <>
             {rolEmpleado === UserRole.ADMIN && (
-              <CardOptions
-                url='/dashboard'
-                title='Administrador'
-                image='/page-icons/admin.png'
-                description='Panel de administrador'
-                footer='Ir al panel'
-              />
+              <>
+                <div className='text-2xl font-bold text-center text-sky-800 dark:text-sky-200'>
+                  Bienvenido {usuario?.nombre}
+                </div>
+                <p className='font-bold text-center'>
+                  Tienes el Rol de {rolEmpleado}
+                </p>
+                <CardOptions
+                  url='/dashboard'
+                  title='Administrador'
+                  image='/page-icons/admin.png'
+                  description='Panel de administrador'
+                  footer='Ir al panel'
+                />
+              </>
             )}
             {rolEmpleado === UserRole.MESERO && (
               <>
+                <div className='text-2xl font-bold text-center text-sky-800 dark:text-sky-200'>
+                  Bienvenido {usuario?.nombre}
+                </div>
+                <p className='font-bold text-center'>
+                  Tienes el Rol de {rolEmpleado}
+                </p>
                 <CardOptions
                   url='/menu/productos'
                   title='Productos'
@@ -61,25 +75,31 @@ export const RolOptions = () => {
                 />
               </>
             )}
-            {rolEmpleado === UserRole.CAJERO && (
-              <CardOptions
-                url='/cajero/dashboard'
-                title='Cajero'
-                image='/page-icons/cashier.png'
-                description='Panel de cajero'
-                footer='Ir al panel'
-              />
-            )}
-            {rolEmpleado === UserRole.COCINERO && (
-              <CardOptions
-                url='/cocinero/dashboard'
-                title='Cocinero'
-                image='/page-icons/chef.png'
-                description='Panel de cocinero'
-                footer='Ir al panel'
-              />
+            {rolEmpleado === UserRole.USER && (
+              <>
+                <div className='text-2xl font-bold text-center text-sky-800 dark:text-sky-200'>
+                  Bienvenido {usuario?.nombre}
+                </div>
+                <p className='font-bold text-center'>
+                  Tienes el Rol de {rolEmpleado}
+                </p>
+                <p className='text-xl font-bold text-center text-teal-700 dark:text-teal-300'>
+                  por el momento el Usuario comun no tiene opciones
+                </p>
+              </>
             )}
           </>
+        ) : (
+          <div className='flex flex-col justify-center items-center mt-20'>
+            <div className='text-2xl font-bold text-center text-red-800 dark:text-red-200'>
+              Inicia sesión o registrate para ver tus opciones
+            </div>
+            <img
+              src='/assets/online-store-img.svg'
+              alt='Tienda'
+              className='w-full h-full'
+            />
+          </div>
         )}
       </div>
     </>
